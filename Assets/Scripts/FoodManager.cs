c7c362b52dca2a58c568d52b23e46624238c61d0
Shashwat <3dartist.shashwat@gmail.com>
2025-01-17 11:50:33 +0530

    Feature_1 - Player movement

---
+++ Assets/Scripts/FoodManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodManager : MonoBehaviour
+{
+    public BoxCollider2D gridArea;
+
+}
---
+++ Assets/Scripts/SnakeController.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour
+{
+    //private Vector2 direction = Vector2.right;
+    //public float speed = 5f;
+
+
+    //void Update()
+    //{
+    //    if(Input.GetKeyDown(KeyCode.W))
+    //    {
+    //        direction = Vector2.up;
+    //    }
+    //    else if (Input.GetKeyDown(KeyCode.S))
+    //    {
+    //        direction = Vector2.down;
+    //    }
+    //    else if (Input.GetKeyDown(KeyCode.A))
+    //    {
+    //        direction = Vector2.left;
+    //    }
+    //    else if (Input.GetKeyDown(KeyCode.D))
+    //    {
+    //        direction = Vector2.right;
+    //    }
+    //}
+
+    //private void FixedUpdate()
+    //{
+    //    this.transform.position = new Vector3(
+    //        Mathf.Round(this.transform.position.x) + direction.x * speed * Time.deltaTime,
+    //        Mathf.Round(this.transform.position.y) + direction.y * speed * Time.deltaTime,
+    //        0.00f
+    //     );
+
+    //}
+    
+
+
+   
+    public float moveSpeed = 5f;        
+    private Vector2 direction = Vector2.right;   
+    private Rigidbody2D rb2d;
+    private Vector2 targetPosition;
+    void Start()
+    {
+        
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            direction = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            direction = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            direction = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            direction = Vector2.right;
+        }
+        if (Vector2.Distance(transform.position, targetPosition) < 0.1f)
+        {
+            // Snap the player's position to the nearest grid point
+            targetPosition = new Vector2(
+                Mathf.Round(transform.position.x) + direction.x,
+                Mathf.Round(transform.position.y) + direction.y
+                );
+        }
+    }
+
+        void FixedUpdate()
+        {
+
+            rb2d.velocity = direction * moveSpeed;
+        }
+
+}
+
+    
